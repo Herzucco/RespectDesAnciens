25ed5d1afef5367dd2e716435b2b0f7129a0cbf5
Herzucco <a.carta@student.isartdigital.com>
2015-06-04 14:07:33 +0200

    no message

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFollower : MonoBehaviour {
+	public List<Transform> paths;
+	public float speed;
+	public bool randomChoosing;
+
+	private int currentIndex;
+
+	void Update(){
+		CheckCurrentDistance ();
+
+		transform.position = Vector3.MoveTowards (transform.position, paths [currentIndex].position, speed * Time.deltaTime);
+	}
+
+	void CheckCurrentDistance(){
+		if(Vector2.Distance(transform.position, paths[currentIndex].position) <= 0.1f){
+			if(randomChoosing){
+				currentIndex = Random.Range(0, paths.Count);
+			}else{
+				if(currentIndex >= paths.Count-1){
+					currentIndex = 0;
+				}else{
+					currentIndex++;
+				}
+			}
+		}
+	}
+}
