6b21b5fd4e722f91a0794a0db2900c3a8063db6b
Herzucco <a.carta@student.isartdigital.com>
2015-06-04 14:34:28 +0200

    z^fke^f

---
+++ Assets/Scripts/OldingGuy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldingGuy : MonoBehaviour {
+	public Color baseColor = Color.blue;
+	public Color targetColor = Color.red;
+	public float lerpingSpeed;
+
+	void Awake(){
+		GetComponent<Renderer> ().material.color = baseColor;
+	}
+
+	void Update(){
+		GetComponent<Renderer> ().material.color = Color.Lerp (GetComponent<Renderer> ().material.color, targetColor, lerpingSpeed * Time.deltaTime);
+	}
+}
