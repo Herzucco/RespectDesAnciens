899b39a62ad78a7913aa3fb4a0cf5ea742e91ac7
Herzucco <a.carta@student.isartdigital.com>
2015-06-04 14:14:01 +0200

    lalala

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+	public float distance;
+	public int damages;
+	public float frequency;
+
+	private bool canShoot;
+	void Start(){
+		StartCoroutine (ShootFrequency ());
+	}
+
+	public void Attack(Enemy mob){
+		if (canShoot) {
+			mob.life -= damages;
+		}
+	}
+
+	IEnumerator ShootFrequency(){
+		for (;;) {
+			canShoot = true;
+			yield return new WaitForSeconds(frequency);
+		}
+	}
+}
