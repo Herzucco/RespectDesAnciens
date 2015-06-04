500e2d8532a9599d0e6c671094f7987875cc27c6
breizhpanda <kieffer41@hotmail.fr>
2015-06-04 13:48:10 +0200

    player Move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 150f;
+	private Vector3 target;
+
+	public PlayerWeapon currentWeapon;
+	void Start () {
+		target = transform.position;
+	}
+	
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}    
+
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour {
+
+	public float range;
+	public float dmg;
+	public float attackSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
